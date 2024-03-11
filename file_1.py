f5545df06a36cdded1373b074d64fd2c266b6338
afzalshaik0241 <162981755+afzalshaik0241@users.noreply.github.com>
2024-03-11 15:57:22 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,136 @@
+'''
+print("hello world")
+---> Commenting
+1.)Commenting is used for making documentation in a python file
+2.)When the line is commented, that line will not be considered
+as a part of the code
+3.)When the program is executed,commented line will not be in a part
+of execution
+---> Note;-
+Single line commenting can be done using # at the begining of the line
+Eg:-
+        #print("hello world")
+
+Multiple line commenting can be done using code.capitalize The lines between the
+quotes will be commented.
+Eg:-
+
+'''
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+'''
+'''
+
+import this
+print(this)
+
+a=8
+b=6
+# import is a keyword which is used to communicated between the phthon files
+
+import antigravity
+'''
+---> Bargained characters
+
+controls and data structures->ABC language
+Strings and its formats-> from C
+Regular expressions-> Pearl
+Exception handling-> Module 3
+Magic methods from->fortron
+'''
+
+'''
+--->What kind of language is python
+Python is a
+-> Object oriented Programming language
+-> Dynamically typed language
+-> Interpreted language
+-> Compiled language
+'''
+
+'''
+---> Flavours of Python
+-> Cpython - Written in C,Most common Implementation of python
+-> Jython - Java implementation of Python
+-> IronPython - Implementation of the python programming language
+                which is tightly with .NET.
+-> brython - Browser python, runs in browers
+-> RubyPython - Bridge between Python and Ruby interpreters
+-> MicroPython - Runs in microcontrollers.
+'''
+
+'''
+---> print(object, sep, end, file, flush)
+print has five parameters
+
+object---> the value to be printed is object
+sep-->separate operator
+Eg:-
+    a = 8
+    b = 9
+    c = 89
+print( a, b, c  sep='$')
+
+end--> Which is used to add the string at the end of value
+It will not keep the command to stay in same line
+Eg:-
+    print(str(a), b, end='%%')
+    print(str(c))
+'''
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
